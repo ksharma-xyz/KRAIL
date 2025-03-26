5336a801d25a4cc162fc22bfc9fb2618e06f3348
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-03-26 22:40:05 +1100

    Remove firebase perf tracing remporarily. wioll be enabled in future prs

--- io/gtfs/src/commonMain/kotlin/xyz.ksharma.krail.io.gtfs/nswstops/StopsProtoParser.kt
+++ io/gtfs/src/commonMain/kotlin/xyz.ksharma.krail.io.gtfs/nswstops/StopsProtoParser.kt
@@ -1,14 +1,8 @@
 package xyz.ksharma.krail.io.gtfs.nswstops
 
 import app.krail.kgtfs.proto.NswStopList
-import dev.gitlive.firebase.Firebase
-import dev.gitlive.firebase.perf.performance
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
-import kotlinx.datetime.Clock
-import kotlinx.datetime.DateTimeUnit
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.until
 import krail.io.gtfs.generated.resources.Res
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import xyz.ksharma.krail.core.log.log
@@ -24,36 +18,17 @@ class StopsProtoParser(
      */
     @OptIn(ExperimentalResourceApi::class)
     override suspend fun parseAndInsertStops() = withContext(ioDispatcher) {
-        val trace = Firebase.performance.newTrace("parseNswStops")
-        trace.start()
-        var start = Clock.System.now()
-
         sandook.clearNswStopsTable()
         sandook.clearNswProductClassTable()
 
         val byteArray = Res.readBytes("files/NSW_STOPS.pb")
         val decodedStops = NswStopList.ADAPTER.decode(byteArray)
-        trace.stop()
-
-        var duration = start.until(
-            Clock.System.now(), DateTimeUnit.MILLISECOND,
-            TimeZone.currentSystemDefault(),
-        )
-        log("Decoded #Stops: ${decodedStops.nswStops.size} - duration: $duration ms")
 
         log("Start inserting stops. Currently ${sandook.stopsCount()} stops in the database")
-        start = Clock.System.now()
         insertStopsInTransaction(decodedStops)
-        duration = start.until(
-            Clock.System.now(), DateTimeUnit.MILLISECOND, TimeZone.currentSystemDefault()
-        )
-        log("Inserted #Stops: ${decodedStops.nswStops.size} in duration: $duration ms")
     }
 
     private suspend fun insertStopsInTransaction(decoded: NswStopList) = withContext(ioDispatcher) {
-        val trace = Firebase.performance.newTrace("insertNSWStops")
-        trace.start()
-        val start = Clock.System.now()
         sandook.insertTransaction {
             decoded.nswStops.forEach { nswStop ->
                 sandook.insertNswStop(
@@ -70,11 +45,5 @@ class StopsProtoParser(
                 }
             }
         }
-
-        val duration = start.until(
-            Clock.System.now(), DateTimeUnit.MILLISECOND, TimeZone.currentSystemDefault(),
-        )
-        log("Inserted ${decoded.nswStops.size} stops in a single transaction in $duration ms")
-        trace.stop()
     }
 }

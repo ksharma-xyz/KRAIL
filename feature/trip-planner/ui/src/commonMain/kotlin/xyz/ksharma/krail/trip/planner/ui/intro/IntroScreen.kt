b46e3f71dade2cf2c4c63cd2a80c79f78cef0437
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-04-14 08:48:51 +1000

    Make button fix to bottom

--- feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/intro/IntroScreen.kt
+++ feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/intro/IntroScreen.kt
@@ -10,14 +10,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithContent
@@ -26,6 +30,7 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import xyz.ksharma.krail.taj.components.Button
 import xyz.ksharma.krail.taj.components.Text
 import xyz.ksharma.krail.taj.theme.KrailTheme
 import xyz.ksharma.krail.trip.planner.ui.state.intro.IntroState
@@ -39,58 +44,82 @@ fun IntroScreen(
     modifier: Modifier = Modifier,
     onEvent: (IntroUiEvent) -> Unit = {},
 ) {
-    Column(modifier = modifier.fillMaxSize().systemBarsPadding()) {
-        Text(
-            text = "Intro Screen",
-            style = KrailTheme.typography.title,
-            modifier = Modifier.padding(16.dp),
-        )
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = KrailTheme.colors.surface)
+            .statusBarsPadding(),
+    ) {
+        Column(modifier = Modifier.align(Alignment.TopCenter)) {
+            Text(
+                text = "Intro Screen",
+                style = KrailTheme.typography.title,
+                modifier = Modifier.padding(16.dp),
+            )
 
-        Spacer(modifier = Modifier.height(48.dp))
+            Spacer(modifier = Modifier.height(32.dp))
 
-        val pagerState = rememberPagerState(pageCount = { 5 })
-        val colors = listOf(Color.Red, Color.Blue, Color.Green, Color.Yellow, Color.Magenta)
+            val pagerState = rememberPagerState(pageCount = { 5 })
+            val colors = listOf(Color.Red, Color.Blue, Color.Green, Color.Yellow, Color.Magenta)
 
-        BoxWithConstraints(Modifier.fillMaxWidth()) {
-            val screenHeight = maxHeight
+            BoxWithConstraints(Modifier.fillMaxWidth()) {
+                val screenHeight = maxHeight
 
-            val selectedHeight = screenHeight * 0.80f
-            val unselectedHeight = screenHeight * 0.6f
+                val selectedHeight = screenHeight * 0.80f
+                val unselectedHeight = screenHeight * 0.6f
 
-            HorizontalPager(
-                state = pagerState,
-                contentPadding = PaddingValues(horizontal = 64.dp),
-                pageSpacing = 20.dp,
-                modifier = Modifier.fillMaxWidth()
-            ) { pageNumber ->
+                HorizontalPager(
+                    state = pagerState,
+                    contentPadding = PaddingValues(horizontal = 64.dp),
+                    pageSpacing = 20.dp,
+                    modifier = Modifier.fillMaxWidth()
+                ) { pageNumber ->
 
-                val pageOffset = pagerState.calculateCurrentOffsetForPage(pageNumber).absoluteValue
-                val animatedHeight by animateDpAsState(
-                    targetValue = lerp(selectedHeight, unselectedHeight, min(1f, pageOffset)),
-                    label = "cardHeight"
-                )
+                    val pageOffset =
+                        pagerState.calculateCurrentOffsetForPage(pageNumber).absoluteValue
+                    val animatedHeight by animateDpAsState(
+                        targetValue = lerp(selectedHeight, unselectedHeight, min(1f, pageOffset)),
+                        label = "cardHeight"
+                    )
 
-                val scale = lerp(1f, 0.9f, min(1f, pageOffset))
+                    val scale = lerp(1f, 0.9f, min(1f, pageOffset))
 
-                val greyOverlayAlpha = min(1f, pageOffset * 1.2f) // Gradual tinting
-                val greyOverlay = Color(0xFF888888).copy(alpha = greyOverlayAlpha)
+                    val greyOverlayAlpha = min(0.6f, pageOffset * 1.2f) // Gradual tinting
+                    val greyOverlay = Color(0xFF888888).copy(alpha = greyOverlayAlpha)
 
-                Box(
-                    modifier = Modifier
-                        .zIndex(1f - pageOffset)
-                        .height(animatedHeight)
-                        .graphicsLayer {
-                            scaleX = scale
-                            scaleY = scale
-                        }
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(24.dp))
-                        .background(colors[pageNumber % colors.size])
-                        .drawWithContent {
-                            drawContent()
-                            drawRect(greyOverlay) // overlays a semi-transparent grey
-                        }
-                )
+                    Column(
+                        modifier = Modifier
+                            .zIndex(1f - pageOffset)
+                            .height(animatedHeight)
+                            .graphicsLayer {
+                                scaleX = scale
+                                scaleY = scale
+                            }
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(24.dp))
+                            .background(colors[pageNumber % colors.size])
+                            .drawWithContent {
+                                drawContent()
+                                drawRect(greyOverlay) // overlays a semi-transparent grey
+                            }
+                            .verticalScroll(rememberScrollState())
+                    ) {
+                        Text("Page $pageNumber", modifier = Modifier.padding(16.dp))
+                    }
+                }
+            }
+        }
+
+        Column(
+            modifier = Modifier.align(Alignment.BottomCenter)
+                .navigationBarsPadding()
+                .padding(bottom = 10.dp)
+        ) {
+            Button(
+                onClick = { },
+                modifier = Modifier.padding(horizontal = 24.dp, vertical = 10.dp),
+            ) {
+                Text(text = "Let's #KRAIL")
             }
         }
     }

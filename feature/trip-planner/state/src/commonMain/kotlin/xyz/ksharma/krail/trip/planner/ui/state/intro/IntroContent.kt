8c2b3f167cdb46abbeb6d2e639927b4f334e3c1b
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-04-14 13:56:40 +1000

    Create UI for the IntroContent pages for IntroScreen horizontal pager

---
+++ feature/trip-planner/state/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/state/intro/IntroContent.kt
@@ -0,0 +1,41 @@
+package xyz.ksharma.krail.trip.planner.ui.state.intro
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import xyz.ksharma.krail.taj.components.Text
+
+@Composable
+fun IntroContentSaveTrips(modifier: Modifier = Modifier) {
+
+    Text("JUST\nONE TAP\nTHAT'S IT")
+}
+
+@Composable
+fun IntroContentRealTime(modifier: Modifier = Modifier) {
+    Text("FASTEST\nROUTES\nEVERYTIME")
+}
+
+@Composable
+fun IntroContentAlerts(modifier: Modifier = Modifier) {
+    Text("WE\nRESPECT\nYOUR TIME")
+}
+@Composable
+fun IntroContentPlanTrip(modifier: Modifier = Modifier) {
+    Text("WE\nCAN TELL\nTHE FUTURE")
+
+}
+
+@Composable
+fun IntroContentInviteFriends(modifier: Modifier = Modifier) {
+    Text("RIDING SOLO\nNAH THANKS")
+
+
+    Text("LET'S\nKRAIL\nTOGETHER")
+}
+
+@Composable
+fun IntroContentSelectMode(modifier: Modifier = Modifier) {
+    Text("TRAIN, BUS\nOR BOTH\nYOUR CHOICE")
+}
+
+

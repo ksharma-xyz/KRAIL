ab242c8ccb20273282e3319ec52d80148562b5cc
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-04-15 15:27:19 +1000

    bb

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,8 +5,8 @@ android {
 
     defaultConfig {
         applicationId = "xyz.ksharma.krail"
-        versionCode = 81
-        versionName = "1.4.0"
+        versionCode = 84
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

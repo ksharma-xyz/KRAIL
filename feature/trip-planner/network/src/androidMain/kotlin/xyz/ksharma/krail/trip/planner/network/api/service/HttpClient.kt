2a5ea334bd8f4cb887b1c6a5f6b08677e60c1739
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-05-10 15:11:00 +1000

    Fix Journey Cache Issue

--- feature/trip-planner/network/src/androidMain/kotlin/xyz/ksharma/krail/trip/planner/network/api/service/HttpClient.kt
+++ feature/trip-planner/network/src/androidMain/kotlin/xyz/ksharma/krail/trip/planner/network/api/service/HttpClient.kt
@@ -4,7 +4,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
@@ -33,7 +32,12 @@ actual fun httpClient(
             coroutineScope.launch {
                 if (appInfoProvider.getAppInfo().isDebug) {
                     level = LogLevel.BODY
-                    logger = Logger.ANDROID
+                    logger = object : Logger {
+                        override fun log(message: String) {
+                            // Package name is used to avoid collision with the overriden Log method
+                            xyz.ksharma.krail.core.log.log(message)
+                        }
+                    }
                     sanitizeHeader { header -> header == HttpHeaders.Authorization }
                 } else {
                     level = LogLevel.NONE

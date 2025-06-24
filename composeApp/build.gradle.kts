a1101eb14e40334445edb321b70aa487cff1fa80
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-06-24 14:43:55 +1000

    bump v post release of 1.7.1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,7 +5,7 @@ android {
 
     defaultConfig {
         applicationId = "xyz.ksharma.krail"
-        versionCode = 103
+        versionCode = 104
         versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

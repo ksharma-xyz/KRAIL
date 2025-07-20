e19e895814709248c09b15bba49b7463c4bc910b
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-07-20 23:10:34 +1000

    automate plan trip

--- feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/intro/IntroContentPlanTrip.kt
+++ feature/trip-planner/ui/src/commonMain/kotlin/xyz/ksharma/krail/trip/planner/ui/intro/IntroContentPlanTrip.kt
@@ -6,18 +6,14 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberTimePickerState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 import xyz.ksharma.krail.core.datetime.rememberCurrentDateTime
 import xyz.ksharma.krail.taj.LocalThemeColor
 import xyz.ksharma.krail.taj.hexToComposeColor
@@ -30,29 +26,48 @@ import xyz.ksharma.krail.trip.planner.ui.state.datetimeselector.JourneyTimeOptio
 @Composable
 fun IntroContentPlanTrip(
     tagline: String,
-    style: String, // hexCode - // todo - see if it can be color instead.
+    style: String, // hexCode
     modifier: Modifier = Modifier,
     onInteraction: () -> Unit = {},
 ) {
-    Column(
-        modifier = modifier
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.SpaceBetween,
-    ) {
-        Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
-            var journeyTimeOption by remember { mutableStateOf(JourneyTimeOptions.LEAVE) }
-            val currentDateTime = rememberCurrentDateTime()
-            val timePickerState = rememberTimePickerState(
-                initialHour = currentDateTime.hour,
-                initialMinute = currentDateTime.minute,
-                is24Hour = false,
-            )
+    val density = LocalDensity.current
 
-            val density = LocalDensity.current
-            val themeColor = rememberSaveable { mutableStateOf(TransportMode.Coach().colorCode) }
+    // Only automate if font scale is small
+    if (density.fontScale < 1.5f) {
+        val allOptions = JourneyTimeOptions.entries.toTypedArray()
+        var journeyTimeOption by remember { mutableStateOf(JourneyTimeOptions.LEAVE) }
+        val currentDateTime = rememberCurrentDateTime()
+        val timeStates = listOf(
+            10 to 10, // 10:10 AM
+            currentDateTime.hour to currentDateTime.minute // system time
+        )
+        var timeStep by remember { mutableStateOf(0) }
+        val timePickerState = rememberTimePickerState(
+            initialHour = timeStates[timeStep].first,
+            initialMinute = timeStates[timeStep].second,
+            is24Hour = false,
+        )
 
-            if (density.fontScale < 1.5f) {
-                // Not required to display if font size is large.
+        // Automate toggling
+        LaunchedEffect(Unit) {
+            var optionStep = 0
+            while (true) {
+                journeyTimeOption = allOptions[optionStep % allOptions.size]
+                timeStep = (optionStep % timeStates.size)
+                timePickerState.hour = timeStates[timeStep].first
+                timePickerState.minute = timeStates[timeStep].second
+                onInteraction()
+                optionStep++
+                delay(2500)
+            }
+        }
+
+        Column(
+            modifier = modifier
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
                 JourneyTimeOptionsGroup(
                     selectedOption = journeyTimeOption,
                     themeColor = style.hexToComposeColor(),
@@ -61,27 +76,27 @@ fun IntroContentPlanTrip(
                         onInteraction()
                     },
                 )
+
+                CompositionLocalProvider(
+                    LocalThemeColor provides rememberSaveable { mutableStateOf(TransportMode.Coach().colorCode) },
+                    LocalDensity provides Density(
+                        (density.density - 0.6f).coerceIn(1.5f, 2.5f),
+                        fontScale = 1f,
+                    ),
+                ) {
+                    TimeSelection(
+                        timePickerState = timePickerState,
+                        displayTitle = false,
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                    )
+                }
             }
 
-            CompositionLocalProvider(
-                LocalThemeColor provides themeColor,
-                LocalDensity provides Density(
-                    (density.density - 0.6f).coerceIn(1.5f, 2.5f),
-                    fontScale = 1f,
-                ),
-            ) {
-                TimeSelection(
-                    timePickerState = timePickerState,
-                    displayTitle = false,
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                )
-            }
+            TagLineWithEmoji(
+                tagline = tagline,
+                emoji = "\uD83D\uDD2E",
+                tagColor = style.hexToComposeColor()
+            )
         }
-
-        TagLineWithEmoji(
-            tagline = tagline,
-            emoji = "\uD83D\uDD2E",
-            tagColor = style.hexToComposeColor()
-        )
     }
 }

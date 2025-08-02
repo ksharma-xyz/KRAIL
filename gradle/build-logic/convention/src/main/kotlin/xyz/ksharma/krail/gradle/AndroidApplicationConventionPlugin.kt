769aab701cca8fcb77beb079f745a948cd0b2cd3
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-08-02 10:07:09 +1000

    bump version

--- gradle/build-logic/convention/src/main/kotlin/xyz/ksharma/krail/gradle/AndroidApplicationConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/kotlin/xyz/ksharma/krail/gradle/AndroidApplicationConventionPlugin.kt
@@ -15,7 +15,7 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
 
             androidAppExtension().apply {
                 defaultConfig {
-                    versionCode = findProperty("versionCode")?.toString()?.toInt() ?: 109
+                    versionCode = findProperty("versionCode")?.toString()?.toInt() ?: 110
                     versionName = "1.7.5"
                 }
             }

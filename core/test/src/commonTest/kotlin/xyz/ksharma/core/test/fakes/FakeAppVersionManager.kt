a935be1d2f64007219c92d50ebda0118c12baaf9
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-08-25 15:19:05 +1000

    update UT

--- core/test/src/commonTest/kotlin/xyz/ksharma/core/test/fakes/FakeAppVersionManager.kt
+++ core/test/src/commonTest/kotlin/xyz/ksharma/core/test/fakes/FakeAppVersionManager.kt
@@ -3,10 +3,11 @@ package xyz.ksharma.core.test.fakes
 import xyz.ksharma.krail.core.appversion.AppVersionManager
 import xyz.ksharma.krail.core.appversion.AppVersionUpdateState
 
-class FakeAppVersionManager: AppVersionManager {
+class FakeAppVersionManager : AppVersionManager {
 
     var versionUpdateState: AppVersionUpdateState = AppVersionUpdateState.UpToDate
     var mockCurrentVersion: String = "1.0.0"
+    var mockUpdateCopy: AppVersionManager.AppVersionUpdateCopy? = null
 
     override suspend fun checkForUpdates(): AppVersionUpdateState {
         return versionUpdateState
@@ -14,5 +15,9 @@ class FakeAppVersionManager: AppVersionManager {
 
     override fun getCurrentVersion(): String {
         return mockCurrentVersion
+    }
+
+    override suspend fun getUpdateCopy(): AppVersionManager.AppVersionUpdateCopy? {
+        return mockUpdateCopy
     }
 }

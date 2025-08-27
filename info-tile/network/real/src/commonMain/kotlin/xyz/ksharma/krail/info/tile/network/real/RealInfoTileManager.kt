78229f5dcd6a79a45ccf1b76769abbafae7996aa
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-08-27 16:05:33 +1000

    addd FlagValue.toInfoTileList(): List<InfoTileData>

--- info-tile/network/real/src/commonMain/kotlin/xyz/ksharma/krail/info/tile/network/real/RealInfoTileManager.kt
+++ info-tile/network/real/src/commonMain/kotlin/xyz/ksharma/krail/info/tile/network/real/RealInfoTileManager.kt
@@ -3,9 +3,15 @@ package xyz.ksharma.krail.info.tile.network.real
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toImmutableList
+import kotlinx.serialization.json.Json
 import xyz.ksharma.krail.core.appinfo.AppInfoProvider
 import xyz.ksharma.krail.core.appversion.AppVersionManager
 import xyz.ksharma.krail.core.log.log
+import xyz.ksharma.krail.core.log.logError
+import xyz.ksharma.krail.core.remote_config.RemoteConfigDefaults
+import xyz.ksharma.krail.core.remote_config.flag.Flag
+import xyz.ksharma.krail.core.remote_config.flag.FlagKeys
+import xyz.ksharma.krail.core.remote_config.flag.FlagValue
 import xyz.ksharma.krail.info.tile.network.api.InfoTileManager
 import xyz.ksharma.krail.info.tile.network.api.InfoTileManager.Companion.MAX_INFO_TILE_COUNT
 import xyz.ksharma.krail.info.tile.network.api.db.isInfoTileDismissed
@@ -19,7 +25,10 @@ class RealInfoTileManager(
     private val appInfoProvider: AppInfoProvider,
     private val platformOps: PlatformOps,
     private val preferences: SandookPreferences,
+    private val flag: Flag,
 ) : InfoTileManager {
+
+    private val list = flag.getFlagValue(FlagKeys.INFO_TILES.key).toInfoTileList()
 
     override suspend fun getInfoTiles(): List<InfoTileData> {
         // todo -
@@ -72,5 +81,32 @@ class RealInfoTileManager(
     private fun isKeyNotInDismissedTiles(key: String): Boolean {
         log("Checking if info tile key '$key' is not in dismissed tiles.")
         return !preferences.isInfoTileDismissed(key)
+    }
+
+    /**
+     * Convert the FlagValue to a list of InfoTileData.
+     * If the FlagValue is not a JsonValue, use the default value from RemoteConfigDefaults.
+     * If decoding fails, log the error and return an empty list.
+     *
+     * Note: This function assumes that the JSON structure matches the InfoTileData data class.
+     * If the structure changes, this function may need to be updated accordingly.
+     * Also, the same model object is being used for parsing from remote config and for UI state,
+     * for less boilerplate and low maintenance.
+     *
+     * @return List of InfoTileData
+     */
+    private fun FlagValue.toInfoTileList(): List<InfoTileData> {
+        val jsonStr = when (this) {
+            is FlagValue.JsonValue -> this.value
+            else -> RemoteConfigDefaults.getDefaults()
+                .firstOrNull { it.first == FlagKeys.INFO_TILES.key }?.second as? String ?: "[]"
+        }
+
+        return try {
+            Json.decodeFromString<List<InfoTileData>>(jsonStr)
+        } catch (e: Exception) {
+            logError("Error decoding info tiles json : $jsonStr", e)
+            emptyList()
+        }
     }
 }

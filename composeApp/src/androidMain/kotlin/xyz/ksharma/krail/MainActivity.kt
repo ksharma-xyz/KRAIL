0e531090d3236f02d60fb169988b66db099474e6
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-10-06 16:06:46 +1100

    Use ComponentActivity only, eceb though it will not support AppCompatDelegate.setDefaultNightMode(nightMode)

--- composeApp/src/androidMain/kotlin/xyz/ksharma/krail/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/xyz/ksharma/krail/MainActivity.kt
@@ -1,12 +1,12 @@
 package xyz.ksharma.krail
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()

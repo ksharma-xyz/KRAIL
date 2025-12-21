3d1908e810efdd4c0e5240a690bbb58cb3ffecd5
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-12-21 13:37:33 +1100

    Bump version from 1.10.0 to 1.11.0 and reset iOS build number (#1244)
    ### TL;DR
    
    Bump version from 1.10.0 to 1.11.0 for both Android and iOS apps.
    
    ### What changed?
    
    - Updated Android app version name from 1.10.0 to 1.11.0 in `AndroidApplicationConventionPlugin.kt`
    - Updated iOS app version from 1.10.0 to 1.11.0 in `Info.plist`
    - Reset iOS build number from 7 to 1 in `Info.plist`
    
    ### How to test?
    
    - Build the Android app and verify the version name is 1.11.0 in the app info
    - Build the iOS app and verify the version is 1.11.0 with build number 1
    
    ### Why make this change?
    
    Preparing for the next release by incrementing the version number to reflect new features and changes that will be included in version 1.11.0.

--- gradle/build-logic/convention/src/main/kotlin/xyz/ksharma/krail/gradle/AndroidApplicationConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/kotlin/xyz/ksharma/krail/gradle/AndroidApplicationConventionPlugin.kt
@@ -16,7 +16,7 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
             androidAppExtension().apply {
                 defaultConfig {
                     versionCode = findProperty("versionCode")?.toString()?.toInt() ?: 115
-                    versionName = "1.10.0"
+                    versionName = "1.11.0"
                 }
             }
 
